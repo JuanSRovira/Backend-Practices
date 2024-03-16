1f0b2cb90be35b17024186c16bedeaa6efb9bbe4
JuanSRovira <narukankid@gmail.com>
2024-03-15 23:44:28 -0600

    add Nodemon & dev/tar script

---
+++ 01-intro-express/server.js
@@ -0,0 +1,22 @@
+// #    Llamar a las bibliotecas de express (importar)
+const  express = require ('express');
+
+//# Crear la instancia express  (crear una aplicacion )
+const app = express()
+
+//# Configurar la app de express(app.use)
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//ERROR MIO: Me aparecio un error porque al usar el app.use(express.json, no coloque los parentesis que lo validan
+// express.json en lugar de *express.json()* )
+
+//#Crear las rutas (app.get)
+app.get ('/', (request, response) => {
+    response.send('Hola Mundo')
+})
+
+//# Inicializar el servidor 
+app.listen(3000, () => {
+    console.log('servidor ejecutandose en el puerto 3000')
+})
