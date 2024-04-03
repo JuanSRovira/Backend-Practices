a0631a23ec6d98bc56ec9ebecc00ef5956e96fce
JuanSRovira <narukankid@gmail.com>
2024-04-03 08:01:18 -0600

    ✨ MVC Update One Home

--- 02-backend/controllers/homeController.js
+++ 02-backend/controllers/homeController.js
@@ -34,6 +34,16 @@ const findOneHome = (req, res) => {
     })
 }
 
+const updateOneHome = (req, res) => {
+  ModelHomes.update(req.params.houseId, req.body)
+    .then((result) => {
+      res.status(200).send(result)
+    })
+    .catch((error) => {
+      res.status(400).send({ message: 'Error updating home', error })
+    })
+}
+
 module.exports = {
-  createHome, findAllHomes, findOneHome
+  createHome, findAllHomes, findOneHome, updateOneHome
 }
